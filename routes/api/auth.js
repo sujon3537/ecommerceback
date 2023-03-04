d798df194bbfd4a6b9b61b0141bb7e9dd74604cd
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2023-03-04 22:25:43 +0600

    user Reg

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,8 +3,19 @@ const _ = express.Router()
 
 // registration
 _.post("/registration", (req, res) => {
-    let {email, name} = req.body
-    res.json({email, name})
+    let {email, fullname, password, isSubscribed} = req.body
+
+    if(!email){
+        return res.json({error: "You must enter an email address"})
+    }
+    if(!fullname){
+        return res.json({error: "You must enter your full name"})
+    }
+    if(!password){
+        return res.json({error: "You must enter a password"})
+    }
+
+    res.json({email, fullname, password, isSubscribed})
 })
 
 module.exports = _
